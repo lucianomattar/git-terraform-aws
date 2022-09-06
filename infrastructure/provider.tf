ce767597e779041eb256cb25b2c0e4a5e23dc505
luciano mattar <lucianomattar@gmail.com>
2022-09-06 10:51:23 -0300

    novo terraform projeto

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 # Centralizar o arquivo de controle de estado do terraform
 terraform {
   backend "s3" {
-    bucket = "terraform-state-igti-lu"
+    bucket = "terraform-state-igti-luc"
     key    = "state/igti/edc/mod1/terraform.tfstate"
     region = "us-east-1"
   }
