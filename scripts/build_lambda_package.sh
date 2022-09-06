c3c6d90707b553c6bfbd2bc6a836b961fcacc485
luciano mattar <lucianomattar@gmail.com>
2022-09-06 15:12:29 -0300

    çl

--- scripts/build_lambda_package.sh
+++ scripts/build_lambda_package.sh
@@ -2,36 +2,9 @@
 
 cd infrastructure
 
-# Declara variavel para reutilização nas validações do diretório
-PACKAGE="package"
+mkdir PACKAGE
 
-# Cria o diretório e instala as dependências da função lambda
-if [ -d $PACKAGE ]
-then
-	echo "O Diretório "$PACKAGE" já existe."
-else
-	echo "============================================="
-	echo "Criando o diretório "$PACKAGE"..."
-	mkdir $PACKAGE
-	echo "O diretório "$PACKAGE" foi criado."
-	echo "============================================="
-fi
-
-# Declara variavel que localiza o requirements com as dependências do projeto
-FILE_REQUIREMENTS=../etl/lambda_requirements.txt
-
-# Verifica se o arquivo lambda_requirements existe
-if [ -f $FILE_REQUIREMENTS ]
-then
-	echo "============================================="
-	echo "Instalando dependências localizadas no "$FILE_REQUIREMENTS""
-	pip install --target ./package -r $FILE_REQUIREMENTS
-	echo "Dependências instaladas com sucesso."
-	echo "============================================="	
-fi
-
-
-cd $PACKAGE
+cd PACKAGE
 
 # Declara variavel que localiza a função lambda para reutilização no código.
 LAMBDA_FUNCTION=../../etl/lambda_function.py
