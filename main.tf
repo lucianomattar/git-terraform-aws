4bef5663d8ca2da8110ead364524c5bb28ca4a8a
luciano mattar <lucianomattar@gmail.com>
2022-09-06 00:51:22 -0300

    lu

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ provider "aws" {
 # Centralizar o arquivo de controle de estado do terraform
 terraform {
   backend "s3" {
-    bucket = "terraform-state-igti-ney"
+    bucket = "terraform-state-igti-lu"
     key    = "state/igti/edc/mod1/terraform.tfstate"
     region = "us-east-2"
   }
